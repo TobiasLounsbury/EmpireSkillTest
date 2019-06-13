05ede05f83db985f0a5d16fdd363ea0a32c0112e
Tobias Lounsbury <tobiaslounsbury@gmail.com>
2019-06-13 12:57:54 -0700

    Added api entries to the routes config

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,34 @@ use Illuminate\Http\Request;
 /*Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });*/
+
+
+// These routes require the user to be logged in
+Route::group(['middleware' => 'auth'], function () {
+  //Start a game
+  Route::post('games/start', 'GameController@store')->name('games.start');
+  //Make a new play in an existing game
+  Route::post('games/{game}/play', 'GameController@play')->name('games.play');
+});
+
+// These routes require no user to be logged in
+Route::group(['middleware' => 'guest'], function () {
+
+  //Get a list of games
+  Route::get('games/', 'GameController@index')->name('games.list');
+  //Details of a single game
+  Route::get('games/{game}', 'GameController@show')->name('games.detail');
+
+
+  //Player Details
+  Route::get('players/{user}', 'PlayerController@details')->name('player.details');
+  //List of players games
+  Route::get('players/{user}/games', 'PlayerController@games')->name('player.games');
+
+
+  //Get Leaderboard info
+  Route::get('leaderboard', 'Leaderboard@index')->name('leaderboard');
+
+});
+
+
